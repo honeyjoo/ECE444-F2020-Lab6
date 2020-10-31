b214cc920c74505e811fbdf102b345f17d3610dd
ryooyoun <sophia.ryoo@mail.utoronto.ca>
2020-10-30 21:08:08 -0600

    search functionality, delete test

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -76,4 +76,8 @@ def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
-    assert data["status"] == 1 
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1   
